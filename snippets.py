1633a18f744203d6f969b65b5ef62e38bece4130
Itay Feldman <itay1405@gmail.com>
2018-11-12 21:14:52 +0200

    fixed annoying errors with a few lines
    simply added fake variables to stop the errors from showing up
    
    Signed-off-by: Itay Feldman <itay1405@gmail.com>

--- snippets.py
+++ snippets.py
@@ -1,10 +1,16 @@
+import socket
+from datetime import datetime
+
+client_sock = socket.socket()
+client_address = ("127.0.0.1", 5555)
 http_response = \
         """\
     HTTP/1.1 200 OK
 
     Hello, World!
     """
-
+request = ""
+request_http = ['GET', '/', '1.1']
 if 'favicon' in request:
     with open('favicon.ico', 'rb+') as favi_file:
         client_sock.sendall(favi_file.read())
@@ -13,18 +19,12 @@ else:
         client_sock.sendall(html_file.read())
 client_sock.close()
 
-
-
 log_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        log_msg = str(client_address) + ' - [%s] \"%s\"\n' % (log_time, request_http[0].strip('\r\n'))
-        # print log_msg
-        log_file.write(log_msg)
+log_msg = str(client_address) + ' - [%s] \"%s\"\n' % (log_time, request_http[0].strip('\r\n'))
+# print log_msg
+# log_file.write(log_msg)
 
-
-
-#client_sock.send("HTTP/1.1 404 Not Found\r\n"
-                 #"Content-Length: %d\r\n"
-                 #"Content-Type: text/html; charset=UTF-8\r\n"
-                 #"\r\n" % os.stat("NotFound.html").st_size)
-
-
+# client_sock.send("HTTP/1.1 404 Not Found\r\n"
+# "Content-Length: %d\r\n"
+# "Content-Type: text/html; charset=UTF-8\r\n"
+# "\r\n" % os.stat("NotFound.html").st_size)
